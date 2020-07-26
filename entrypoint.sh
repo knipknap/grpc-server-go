2367815026741541d4cbd030d7d1966eb1bb82de
Samuel Abels <knipknap@gmail.com>
2020-07-26 11:28:21 +0000

    change entrypoint start sequence

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-grpcui -bind 0.0.0.0 -plaintext -port ${GRPCUI_PORT:-8080} ${GRPCUI_SERVER:-} &
-./start
+./start &
+sleep 2
+grpcui -bind 0.0.0.0 -plaintext -port ${GRPCUI_PORT:-8080} ${GRPCUI_SERVER:-}
